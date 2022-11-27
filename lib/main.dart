c75958ea6f4b67a3a8fbf6fe5a6f3710cb9601d1
1gnoramus <elenwarface@gmail.com>
2022-11-27 21:30:18 +0600

    sda

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,41 @@ void main() {
 
 class MySpriteTutorial extends FlameGame {
   late SpriteAnimationComponent girlWalking;
+  late SpriteComponent background;
 
   Future<void> onLoad() async {
     super.onLoad();
+
+    background = SpriteComponent()
+      ..sprite = await loadSprite('bg.png')
+      ..size = size;
+    add(background);
+
     final spriteSheet =
         await fromJSONAtlas('spritesheet.png', 'spritesheet.json');
     SpriteAnimation walk =
         SpriteAnimation.spriteList(spriteSheet, stepTime: .1);
     girlWalking = SpriteAnimationComponent()
       ..animation = walk
-      ..position = Vector2(100, 100)
-      ..size = Vector2(200, 200);
+      ..position = Vector2(70, 0)
+      ..size = Vector2(100, 100);
     add(girlWalking);
+  }
+
+  void update(double dt) {
+    super.update(dt);
+    if (girlWalking.x < 100) {
+      girlWalking.y += dt * 40;
+      girlWalking.x += dt * 40;
+    } else if (girlWalking.x > 100 && girlWalking.x < 320) {
+      girlWalking.y += dt * 0;
+      girlWalking.x += dt * 40;
+    } else if (girlWalking.x > 320) {
+      girlWalking.y += dt * 40;
+      girlWalking.x += dt * 0;
+    } else if (girlWalking.x > 320 && girlWalking.y < 200) {
+      girlWalking.y += dt * 40;
+      girlWalking.x += dt * 0;
+    }
   }
 }
